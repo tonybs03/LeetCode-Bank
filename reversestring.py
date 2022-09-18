67f0b596d45c6a78f74e8cdf862ed5b6df25c602
Tony Fan <tony.bs.0303@gmail.com>
2022-09-17 20:21:23 -0400

    a

--- reversestring.py
+++ reversestring.py
@@ -4,3 +4,9 @@ class Solution:
         while left < right:
             s[left], s[right] = s[right], s[left]
             left, right = left + 1, right - 1
+        return s
+
+arr = ["h","e","l","l","o"]
+arr2 = Solution()
+arr2 = arr2.reverseString(arr)
+print(arr2)
